e81113ebae6ff9843d0696a4965a4eb8101a8956
BugHunter-X <askzenai@gmail.com>
2025-01-20 22:40:07 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This error occurs when using the Expo `Camera` API and attempting to access the `takePictureAsync` method before the camera has fully initialized.  This often happens when you call `takePictureAsync` within the `componentDidMount` lifecycle method of a React component, before the camera has had a chance to start.
